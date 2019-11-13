c2c5db5d934d24ea7c243793cf3a8e40be049961
Matthias Wauer <matthiaswauer@gmail.com>
2019-11-13 17:39:48 +0100

    hci_raw: print err when net_buf_alloc fails

--- subsys/bluetooth/host/hci_raw.c
+++ subsys/bluetooth/host/hci_raw.c
@@ -56,6 +56,8 @@ struct net_buf *bt_buf_get_rx(enum bt_buf_type type, s32_t timeout)
 	if (buf) {
 		net_buf_reserve(buf, CONFIG_BT_HCI_RESERVE);
 		bt_buf_set_type(buf, type);
+	} else {
+		BT_ERR("net_buf_alloc failed: %d, TO: %s", type, timeout);
 	}
 
 	return buf;
